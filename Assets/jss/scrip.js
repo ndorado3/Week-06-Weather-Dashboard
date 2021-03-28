c18b742b11c5fc6896f2a4a8b9216eb1c8295906
nadiadorado@gmail.com <ndorado@uncc.edu>
2021-03-27 22:07:01 -0400

    Added working icons and uv index

--- Assets/jss/scrip.js
+++ Assets/jss/scrip.js
@@ -1,25 +1,22 @@
 //API key from Open Weather
 var apiKey = "be9663d1de43e9ed7444e910843a85df";
-// var urlFiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInput + "&Appid=" + apiKey + "&units=imperial";
 
 //Variables declaration
-var humidityEL = document.querySelector("#humidity");
-var windSpeedEL = document.querySelector("#windSpeed");
-var uvIndexEL = document.querySelector("#uvIndex");
 var cityFormEl = document.querySelector("#city-search-form");
 var containerWeatherEl = document.querySelector("#c-weather");
 var citySearchInputEl = document.querySelector("#c-city");
 var citySearchName = document.querySelector("#s-city");
+var currentWeatherContainer = document.getElementById("c-weather");
 
-var city = "";
-var searchCities = [];
+// var city = "";
+// var searchCities = [];
 
 var formSubmitHandler = function (e) {
   e.preventDefault();
   var city = citySearchName.value.trim();
   if (city) {
     getCurrentCityWeather(city);
-    console.log(city);
+    // console.log(city);
   }
 };
 
@@ -37,15 +34,24 @@ function getCurrentCityWeather(city) {
     .then((data) => {
       //variables declaration for current weather
       var currentWeatherContainer = document.getElementById("c-weather");
-      console.log(data);
+
+      // console.log(data);
       // console.log(data.coord.lat);
       // console.log(data.coord.lon);
 
-      // City name, date and icon
+      // Current weather icon
+      var currentIconEl = document.createElement("img");
+      let iconCurrent = data.weather[0].icon;
+      currentIconEl.setAttribute(
+        "src",
+        "https://openweathermap.org/img/wn/" + iconCurrent + "@2x.png"
+      );
+      currentWeatherContainer.appendChild(currentIconEl);
+      // console.log(data.weather[0].icon);
+
+      // City name, date
       var cityNameEl = document.createElement("h3");
       cityNameEl.classList.add("cityNmae");
-      var iconCurrent = data.weather[0].icon;
-      var urlIcon = "https://openweathermap.org/img/wn/" + iconCurrent + ".png";
       var currentDate = new Date(data.dt * 1000).toLocaleDateString();
       cityNameEl.textContent = data.name + " " + "(" + currentDate + ")";
       currentWeatherContainer.appendChild(cityNameEl);
@@ -65,63 +71,64 @@ function getCurrentCityWeather(city) {
       humidityEl.textContent = "Humidity: " + humidity + "%";
       currentWeatherContainer.appendChild(humidityEl);
 
-      // Wind speed element 
+      // Wind speed element
       var windSpeedEl = document.createElement("p");
       windSpeedEl.classList.add("current");
       var windSpeed = data.wind.speed;
       windSpeedEl.textContent = "Wind Speed: " + windSpeed + "mph";
       currentWeatherContainer.appendChild(windSpeedEl);
 
-      //call getUVindex ()
+      // function for UV index.I have to use onecall API
+      var lat = data.coord.lat;
+      var lon = data.coord.lon;
+      var uvIndexUrl =
+        "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+        lat +
+        "&lon=" +
+        lon +
+        "&exclude=hourly,minutely,daily,alerts&appid=be9663d1de43e9ed7444e910843a85df";
+      fetch(uvIndexUrl)
+        .then((responseUvi) => {
+          return responseUvi.json();
+        })
+        .then((dataUvi) => {
+          // var currentWeatherContainer = document.getElementById("c-weather");
+          // console.log(data);
+          var uvIndexEl = document.createElement("p");
+          uvIndexEl.classList.add("current");
+          var uvIndex = dataUvi.current.uvi;
+          uvIndexEl.textContent = "UV Index : " + uvIndex;
+          currentWeatherContainer.appendChild(uvIndexEl);
+        });
     });
 }
 
-// fucntion for UV index
-
-function getUvIndex() {
-  var apiWeatherURL =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    city +
-    "&appid=" +
-    apiKey;
-  fetch(apiWeatherURL)
+// 5 dyas forecast
+function get5dayForecast(cityid) {
+  var url5DayForecast =
+  "https://api.openweathermap.org/data/2.5/forecast?q=" +
+  cityid +
+  "&Appid=" +
+  apiKey +
+  "&units=imperial";
+  fetch(url5DayForecast)
     .then((response) => {
       return response.json();
     })
     .then((data) => {
-      // displayCityWeather(data, city);
-      console.log(data.main.temp);
-      console.log(data.main.humidity);
-      console.log(data.wind.speed);
-      var temperatureEl = document.createElement("p");
-      temperatureEl.classList.add("current");
+      for (i = 0; i < forecast.length; i++){
+        forescast[i].innerHTML = "";
+        const dateForecast = new Date(data.daily[i + 1].dt * 1000).toLocaleDateString();
+        forecast[i].append(dateForecast);
 
-      temperatureEl.textContent = "temperature" + data.main.temp;
-      var currentWeatherContainer = document.getElementById("c-weather");
-      currentWeatherContainer.appendChild(temperatureEl);
+        //Icon for 5 days forecast 
+        var iconForecast = data.daily[i].weather[0].icon;
+      
 
-      // //   Date element
-      //     var cDateEl = document.createElement("span");
-      //     cDateEl.textContent =
-      //       "(" + moment( data.dt.value).format("MMMM Do YYYY, h:mm:ss a") + ")";
-      //       citySearchInputEl.appendChild(cDateEl);
+      };
+
     });
+
 }
-
-// Displays the current city search weather
-// var displayCityWeather = function (weather, citySearch) {
-//   containerWeatherEl.textContent = "";
-//   citySearchEl.textContent = citySearch;
-
-//   // icon element for current weather
-//   var weatherIcon = document.createElement("img");
-//   weatherIcon.setAttribute("src",`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
-//   citySearchInputEl.appendChild(weatherIcon);
-
-//    //temperature
-//     // temperatureCurrent = weather.main.temp.toFixed(0);
-//     // displayTe
-
-// };
 
 cityFormEl.addEventListener("submit", formSubmitHandler);
